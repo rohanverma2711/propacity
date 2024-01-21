6f79fc4020e7a0716bc7c27aded6fd1cebc56c19
Rohan Kumar <rohan.k@bursysgroup.com>
2024-01-21 13:17:07 +0530

    Changes in gitignore

---
+++ src/apiConfig.js
@@ -0,0 +1,4 @@
+export const googleSheetApi =
+  "https://api.propacity.in/api/v1/webhooks/integration/61cf0d44-1ede-4dfa-b3ce-9834102082/insertLead";
+export const propacityApi =
+  "https://sheet.best/api/sheets/cecdefa6-b8dc-4163-a92d-24dea7795a28";
